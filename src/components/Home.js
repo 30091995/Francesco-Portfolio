49b60913441e8b80a6522daee9d9fe6e2c3d8251
Francesco Saccone <francescosaccone95@live.it>
2020-09-28 17:46:57 +0200

    finished

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
+import "./Home.css"
+import Footer from './Footer';
+
+function Home ()
+{
+    return(
+        <div >
+        <Container fluid className="startFull">
+        <Row className="h-100 align-items-center justify-content-center">
+          <Col xs="10" md="auto" className="fadeIn text-center">
+            <Col className="display-4 text-info">Francesco Saccone</Col>
+            <Col className="h5 text-dark text-monospace my-4">
+              Click the link to check my portfolio
+            </Col>
+            <Row className="my-4 justify-content-center">
+              <Col xs="auto">
+                <Link className="text-info" to="/about">
+                  To my portfolio!
+                </Link>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+      </div>
+      
+    )
+}
+
+export default Home
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavigationBar.css'
+
+function NavigationBar() {
+    return (
+        <div>
+            <Navbar expand="lg" className="box" style={{backgroundColor: "#F8F7F7"}}>
+                <Navbar.Brand className="link-color"><Link to={"/"}>Francesco Saccone</Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link><Link to={"/about"}>About</Link></Nav.Link>
+                    <Nav.Link><Link to={"/projects"} >Projects</Link></Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
+export default NavigationBar
